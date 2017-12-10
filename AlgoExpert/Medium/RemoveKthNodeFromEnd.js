b0cb78a27d83733fd9a5c6cf99fa0d9ddac23f58
Eren Erdogan <ererdogan20@gmail.com>
2017-12-10 16:54:42 -0500

    completed remove kth node

--- AlgoExpert/Medium/RemoveKthNodeFromEnd.js
+++ AlgoExpert/Medium/RemoveKthNodeFromEnd.js
@@ -10,7 +10,38 @@ Sample output: 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 7 -> 8 -> 9
 */
 
 function removeKthNodeFromEnd(head, k) {
-  // Write your code here.
+	// Base Case
+	if (k === 0) return head;
+
+	let lead = head;
+	let trail = head;
+	let count = 0;
+
+	while (count < k) {
+		lead = lead.next;
+		count++;
+	}
+
+	// Edge Case, head needs to be removed
+	if (lead === null) {
+		head.value = head.next.value;
+    head.next = head.next.next;
+    return;
+	}
+
+	// Iterate until lead is at the last node
+	while (lead.next !== null) {
+		lead = lead.next;
+		trail = trail.next;
+	}
+
+	// By pass kth node
+	trail.next = trail.next.next;
+
+	return head;
 }
+
+exports.removeKthNodeFromEnd = removeKthNodeFromEnd;
+
 
 exports.removeKthNodeFromEnd = removeKthNodeFromEnd;
