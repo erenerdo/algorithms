d26285ecb377810589754068d7e51720ee668501
Eren Erdogan <ererdogan20@gmail.com>
2017-12-11 13:47:33 -0500

    completed BST Traversal

--- AlgoExpert/Medium/BSTTraversal.js
+++ AlgoExpert/Medium/BSTTraversal.js
@@ -15,17 +15,33 @@ Sample output (inOrderTraverse): [1, 2, 5, 5, 10, 15, 22]
 Sample output (preOrderTraverse): [10, 5, 2, 1, 5, 15, 22]
 Sample output (postOrderTraverse): [1, 2, 5, 5, 22, 15, 10]
 */
-
 function inOrderTraverse(tree, array) {
-  // Write your code here.
+	if (!tree) return;
+
+	inOrderTraverse(tree.left, array);
+	array.push(tree.value);
+	inOrderTraverse(tree.right, array);
+
+	return array;
 }
 
 function preOrderTraverse(tree, array) {
-  // Write your code here.
+	if (!tree) return;
+
+	array.push(tree.value);
+	preOrderTraverse(tree.left, array);
+	preOrderTraverse(tree.right, array);
+	return array;
 }
 
 function postOrderTraverse(tree, array) {
-  // Write your code here.
+	if (!tree) return;
+
+	postOrderTraverse(tree.left, array);
+	postOrderTraverse(tree.right, array);
+	array.push(tree.value);
+
+	return array;
 }
 
 // Do not edit the line below.
