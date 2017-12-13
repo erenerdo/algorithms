db0df97b5d0547bdb23c5792945dae9c85763f5f
Eren Erdogan <ererdogan20@gmail.com>
2017-12-13 17:33:29 -0500

    working solution O(nm) runtime, O(nm), will try and optimize space

--- AlgoExpert/Medium/LevenshteinDistance.js
+++ AlgoExpert/Medium/LevenshteinDistance.js
@@ -8,39 +8,42 @@ Sample input: "abc", "yabd"
 Sample output: 2 (insert "y"; substitute "c" for "d")
 
 */
+
 function levenshteinDistance(str1, str2) {
-  let i = 0;
-  let j = 0;
-  let count = 0;
+	let levArr = new Array(str2.length + 1).fill(0);
+	levArr = levArr.map(ele => new Array(str1.length + 1).fill(0));
 
-  while (i < str1.length && j < str2.length) {
-    if (str1[i] === str2[j]) {
-      i++;
-      j++;
-    }
-    else if (str2[j + 1] === str1[i]) {
-      j++;
-      count++;
-    }
-    else if (str1[i + 1] === str2[j]) {
-      i++;
-      count++;
-    }
-    else {
-      i++;
-      j++;
-      count++;
-    }
-  }
-  if (i < str1.length) {
-    count += str1.length - i;
-  }
-  if (j < str2.length) {
-    count += str2.length - j;
-  }
 
-  return count;
+	for (let i = 0; i <= str2.length; i++) {
+		levArr[i][0] = i;
+	}
+
+	for (let j = 0; j <= str1.length; j++) {
+		levArr[0][j] = j;
+	}
+
+	for (let row = 1; row <= str1.length; row++) {
+
+		for (let col = 1; col <= str2.length; col++) {
+
+			if (str2[col - 1] === str1[row - 1]) {
+				levArr[col][row] = levArr[col - 1][row - 1];
+			}
+			else {
+				levArr[col][row] = 1 + Math.min(
+					levArr[col - 1][row],
+					levArr[col - 1][row - 1],
+					levArr[col][row - 1]
+				);
+			}
+		}
+	}
+	return levArr[str2.length][str1.length];
 }
+
+// Do not edit the line below.
+exports.levenshteinDistance = levenshteinDistance;
+
 
 // Do not edit the line below.
 exports.levenshteinDistance = levenshteinDistance;
