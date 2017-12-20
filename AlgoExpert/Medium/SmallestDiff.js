96335f22f737ca499146856b4c9207e614940d47
Eren Erdogan <ererdogan20@gmail.com>
2017-12-20 12:02:40 -0500

    Completed smallestdiff

--- AlgoExpert/Medium/SmallestDiff.js
+++ AlgoExpert/Medium/SmallestDiff.js
@@ -8,9 +8,34 @@ Sample input: [-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]
 Sample output: [28, 26]
 
 */
+let a1 = [-1, 5, 10, 20, 28, 3];
+let a2 = [26, 134, 135, 15, 17];
+
+console.log(smallestDifference(a1, a2));
 
 function smallestDifference(arrayOne, arrayTwo) {
-  // Write your code here.
+    arrayOne.sort((a,b) => a - b);
+    arrayTwo.sort((a,b) => a - b);
+
+    let sol = [null, null];
+    let i = arrayOne.length - 1;
+    let j = arrayTwo.length - 1;
+    let smD = Infinity;
+
+    while (i >= 0 && j >= 0) {
+
+        if (Math.abs(arrayOne[i] - arrayTwo[j]) < smD) {
+            smD = Math.abs(arrayOne[i] - arrayTwo[j]);
+            sol[0] = arrayOne[i];
+            sol[1] = arrayTwo[j];
+        }
+
+        if (arrayOne[i] > arrayTwo[j]) i--;
+        else if (arrayOne[i] < arrayTwo[j]) j--;
+        else return sol;
+    }
+
+    return sol;
 }
 
 // Do not edit the line below.
