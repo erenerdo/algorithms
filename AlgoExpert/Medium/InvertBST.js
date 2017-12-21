e7f02f2c5c598ea1adb73ebec1fe9b08a97b274d
Eren Erdogan <ererdogan20@gmail.com>
2017-12-20 19:26:08 -0500

    completed Invert BST

---
+++ AlgoExpert/Medium/InvertBST.js
@@ -0,0 +1,37 @@
+/*
+
+Invert Binary Tree
+
+Write a function that takes in a Binary Tree and inverts it. In other words, the function should swap every left node in the tree for its corresponding (mirrored) right node. Each Binary Tree node has a value stored in a property called "value" and two children nodes stored in properties called "left" and "right," respectively. Children nodes can either be Binary Tree nodes themselves or the None (null) value.
+
+Sample input:
+            1
+          /   \
+        2      3
+      /  \   /  \
+    4    5  6    7
+  /  \
+8     9
+Sample output:
+            1
+          /   \
+        3     2
+      /  \   /  \
+    7    6  5   4
+               / \
+              9   8
+
+*/
+
+function invertBinaryTree(tree) {
+	// Base Case
+  if (tree === null) return;
+
+	// Post Order
+	invertBinaryTree(tree.left);
+	invertBinaryTree(tree.right);
+	// Swap children nodes
+	let temp = tree.left;
+	tree.left = tree.right;
+	tree.right = temp;
+}
