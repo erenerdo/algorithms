e43ca5274fe7634da16489c2719f539af1d069dc
Eren Erdogan <ererdogan20@gmail.com>
2018-01-02 15:01:00 -0500

    Added DFS solution as well

--- LeetCode/Q200_CountIslands.js
+++ LeetCode/Q200_CountIslands.js
@@ -18,9 +18,10 @@ Example 2:
 Answer: 3
 */
 
-/**
- * @param {character[][]} grid
- * @return {number}
+/*
+
+BFS Solution
+
  */
 var numIslands = function(binaryMatrix) {
   let islandCount = 0;
@@ -67,4 +68,47 @@ var numIslands = function(binaryMatrix) {
    let m = matrix[0].length;
    if (row < 0 || row >= n || col < 0 || col >= m || matrix[row][col] != '1') return;
    queue.push([row, col]);
+ }
+
+ /*
+
+ DFS Solution
+
+ */
+
+var numIslands = function(binaryMatrix) {
+  let islandCount = 0;
+
+   for (let row = 0; row < binaryMatrix.length; row++) {
+     for (let col = 0; col < binaryMatrix[0].length; col++) {
+       // If current element is 1, BFS
+       if (binaryMatrix[row][col] == '1') {
+         let n = binaryMatrix.length;
+         let m = binaryMatrix[0].length;
+
+         DFSMark(binaryMatrix, row, col, n, m)
+
+
+         islandCount++;
+       }
+     }
+   }
+   console.log(binaryMatrix)
+   return islandCount;
+ }
+
+ function DFSMark (matrix, r, c, n, m) {
+   if (r < 0 || r >= n || c < 0 || c >= m || !matrix[r].length || matrix[r][c] !== '1') return;
+
+   // Visit and mark to -1
+   matrix[r][c] = '-1';
+
+   // Top
+   DFSMark(matrix, r + 1, c, n, m);
+   // Right
+   DFSMark(matrix, r, c + 1, n, m);
+   // Bottom
+   DFSMark(matrix, r - 1, c, n, m);
+   // Left
+   DFSMark(matrix, r, c - 1, n, m);
  }
