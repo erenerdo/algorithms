dcb77bcabfe5eeaf312468c16d918d0869617f5b
Eren Erdogan <ererdogan20@gmail.com>
2018-01-08 14:17:47 -0500

    Added new pramp questions and LC

---
+++ LeetCode/Explore/Blue/Arrays and Strings/AddBinary.js
@@ -0,0 +1,40 @@
+/**
+Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100".
+
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+*/
+var addBinary = function(a, b) {
+  const sums = [];
+  let i = a.length - 1;
+  let j = b.length - 1;
+  let carry = 0;
+
+  while (i >= 0 || j >= 0 || carry) {
+    const bitA = a[i] !== undefined ? +a[i] : 0;
+    const bitB = b[j] !== undefined ? +b[j] : 0;
+    const sum = bitA + bitB + carry;
+
+    if (sum <= 1) {
+      sums.unshift(sum);
+      carry = 0;
+    }
+    else if (sum === 2) {
+      sums.unshift(0);
+      carry = 1;
+    }
+    else {
+      sums.unshift(1);
+      carry = 1;
+    }
+    i--;
+    j--;
+  }
+  return sums.join('');
+};
