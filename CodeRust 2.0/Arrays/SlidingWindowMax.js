8a0ea734c138e5f665d2712af4fdef228ef51306
Eren Erdogan <ererdogan20@gmail.com>
2018-01-12 10:58:15 -0500

    completed sliding window max

---
+++ CodeRust 2.0/Arrays/SlidingWindowMax.js
@@ -0,0 +1,53 @@
+/*
+Find Maximum in Sliding Window:
+
+Given a large array of integers and a window of size 'w', find the current maximum in the window as the window slides through the entire array.
+*/
+
+function maxSlidingWindow(nums, w) {
+  // Edge case
+   if (w === 0) return [];
+
+   const window = [];
+   const maxes = [];
+
+   // Add first element to the window
+   window.push(new WN(nums[0], 0));
+
+   // Add remaining elements in window to the list
+   for (let i = 1; i < w; i++) {
+     // Remove elements from the tail that are less than the current element
+     removeLowVals(window, nums[i]);
+     window.push(new WN(nums[i], i));
+   }
+   // Add first max to the maxes array
+   maxes.push(window[0].ele);
+
+   // For the remaining windows
+   for (let i = 1; i <= nums.length - w; i++) {
+     let newEle = nums[i + w - 1];
+
+     // Remove elements from the tail that are less than the current element
+     removeLowVals(window, newEle);
+
+     while (window.length && window[0].idx < i) {
+       window.shift();
+     }
+     window.push(new WN(newEle, i + w - 1));
+     maxes.push(window[0].ele);
+   }
+   return maxes;
+ }
+
+ // Helper function
+ function removeLowVals (window, ele) {
+   while (window.length && window[window.length - 1].ele <= ele) {
+       window.pop();
+   }
+ }
+
+ // Window Node Class
+ function WN (ele, idx) {
+   this.ele = ele;
+   this.idx = idx;
+ }
