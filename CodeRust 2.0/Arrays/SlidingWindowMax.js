9785ee5ac836446024df4ca25940e7cea4a2dd6d
Eren Erdogan <ererdogan20@gmail.com>
2018-01-12 11:07:06 -0500

    added an additional solution for sliding window max

--- CodeRust 2.0/Arrays/SlidingWindowMax.js
+++ CodeRust 2.0/Arrays/SlidingWindowMax.js
@@ -51,3 +51,56 @@ function maxSlidingWindow(nums, w) {
    this.ele = ele;
    this.idx = idx;
  }
+
+/*
+
+ Just using indexes:
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, w) {
+  // Edge case
+   if (w === 0) return [];
+
+   const window = [];
+   const maxes = [];
+
+   // Add first element to the window
+   window.push(0);
+
+   // Add remaining elements in window to the list
+   for (let i = 1; i < w; i++) {
+     // Remove elements from the tail that are less than the current element
+     removeLowVals(window, nums[i], nums)
+     window.push(i);
+   }
+   // Add first max to the maxes array
+   maxes.push(nums[window[0]]);
+
+   // For the remaining windows
+   for (let i = 1; i <= nums.length - w; i++) {
+     let newEle = nums[i + w - 1];
+
+     // Remove elements from the tail that are less than the current element
+     removeLowVals(window, newEle, nums)
+
+     while (window.length && window[0] < i) {
+       window.shift();
+     }
+     window.push(i + w - 1)
+     maxes.push(nums[window[0]]);
+   }
+   return maxes;
+ };
+
+ function removeLowVals (window, ele, nums) {
+   while (window.length && nums[window[window.length - 1]] <= ele) {
+       window.pop();
+   }
+ }
+
