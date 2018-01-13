b23ad45f3eb0d6ffd1c206c8237bfe5d71b59194
Eren Erdogan <ererdogan20@gmail.com>
2018-01-13 15:34:09 -0500

    updated low high index

--- CodeRust 2.0/Arrays/LowHighIndex.js
+++ CodeRust 2.0/Arrays/LowHighIndex.js
@@ -9,31 +9,37 @@ Find low/high index
 Given a sorted array of integers, return the low and high index of the given key. Return -1 if not found. The array length can be in millions with lots of duplicates.
 */
 
-function findLowHigh (arr, key) {
+function findLowIndex (nums, target) {
   let lo = 0;
-  let hi = arr.length - 1;
+  let hi = nums.length - 1;
 
   while (lo <= hi) {
     let mid = Math.floor((lo + hi) / 2);
-    if (arr[mid] === key) {
-      lo = mid;
-      hi = mid;
-      while (arr[lo] === key) lo--;
-      while (arr[hi] === key) hi++;
-      return [lo + 1, hi - 1];
-    }
-    else if (arr[mid] < key) {
-      lo = mid + 1;
-    }
-    else {
-      hi = mid - 1;
-    }
+    if (nums[mid] >= target) hi = mid - 1;
+    else lo = mid + 1;
   }
+  if (nums[lo] === target) return lo;
   return -1;
 }
 
-console.log(findLowHigh([1, 2, 5, 5, 5, 5, 5, 5, 5, 5, 20], 1))
-console.log(findLowHigh([1, 2, 5, 5, 5, 5, 5, 5, 5, 5, 20], 2))
-console.log(findLowHigh([1, 2, 5, 5, 5, 5, 5, 5, 5, 5, 20], 5))
-console.log(findLowHigh([1, 2, 5, 5, 5, 5, 5, 5, 5, 5, 20], 20))
-console.log(findLowHigh([1, 2, 5, 5, 5, 5, 5, 5, 5, 5, 20], 15))
+console.log(findLowIndex([1, 2, 4, 5, 5, 5, 5, 5, 6, 6, 7, 8, 11], 5));
+console.log(findLowIndex([1, 2, 4], 2));
+console.log(findLowIndex([1, 2, 4], 3));
+
+
+function findHighIndex (nums, target) {
+  let lo = 0;
+  let hi = nums.length - 1;
+
+  while (lo <= hi) {
+    let mid = Math.floor((lo + hi) / 2);
+    if (nums[mid] > target) hi = mid - 1;
+    else lo = mid + 1;
+  }
+  if (nums[hi] === target) return hi;
+  return -1;
+}
+
+console.log(findHighIndex([1, 2, 4, 5, 5, 5, 5, 5, 6, 6, 7, 8, 11], 5));
+console.log(findHighIndex([1, 2, 4], 2));
+console.log(findHighIndex([1, 2, 4], 3));
---
+++ CodeRust 2.0/Linked Lists/ReverseLL.js
@@ -0,0 +1,7 @@
+/*
+
+Reverse a singly linked list
+Given the pointer/reference to the head of a singly linked list, reverse it and return the pointer/reference to the head of reversed linked list.
+
+*/
+
