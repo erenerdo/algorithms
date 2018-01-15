121f1023fc982abe18125bf5184c1cfc3073328b
Eren Erdogan <ererdogan20@gmail.com>
2018-01-15 17:30:06 -0500

    added reverse LL code

--- CodeRust 2.0/Linked Lists/ReverseLL.js
+++ CodeRust 2.0/Linked Lists/ReverseLL.js
@@ -5,3 +5,15 @@ Given the pointer/reference to the head of a singly linked list, reverse it and 
 
 */
 
+var reverseList = function(head) {
+  let trail = null;
+  let lead = head;
+
+  while (lead !== null) {
+    let nextNode = lead.next;
+    lead.next = trail
+    trail = lead;
+    lead = nextNode;
+  }
+  return trail;
+};
