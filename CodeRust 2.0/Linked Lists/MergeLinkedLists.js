11669cc7ff21d1d34d3a1bb75806e45ff068a816
Eren Erdogan <ererdogan20@gmail.com>
2018-01-15 14:31:32 -0500

    completed merge sorted ll

---
+++ CodeRust 2.0/Linked Lists/MergeLinkedLists.js
@@ -0,0 +1,33 @@
+var mergeTwoLists = function(l1, l2) {
+  // Check for edge cases
+  if (!l1 && !l2) return null;
+  else if (l1 && !l2) return l1;
+  else if (!l1 && l2) return l2;
+
+  let lead1 = l1;
+  let trail1 = null;
+  let lead2 = l2;
+  let trail2 = null;
+
+  while (lead1 !== null && lead2 !== null) {
+    if (lead1.val < lead2.val) {
+      trail1 = lead1;
+      lead1 = lead1.next;
+
+      if (lead1 === null || lead1.val >= lead2.val) {
+        trail1.next = lead2;
+      }
+    }
+    else {
+      trail2 = lead2;
+      lead2 = lead2.next;
+
+      if (lead2 === null || lead2.val > lead1.val) {
+        trail2.next = lead1;
+      }
+    }
+  }
+
+  if (l1.val < l2.val) return l1;
+  return l2;
+};
