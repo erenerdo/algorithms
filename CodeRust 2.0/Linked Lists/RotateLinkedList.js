d5d0932138bf3a690a7642df5bf4453ce10f94fb
Eren Erdogan <ererdogan20@gmail.com>
2018-01-15 11:45:11 -0500

    completed rotate linked list

---
+++ CodeRust 2.0/Linked Lists/RotateLinkedList.js
@@ -0,0 +1,46 @@
+/*
+Rotate a Linked List
+Given head node of a singly linked list and an integer 'n', rotate linked list by 'n'.
+*/
+
+var rotate = function(head, k) {
+  // Edge Case
+  if (!head) return null;
+
+  const length = getLength(head);
+
+  k = k % length;
+  if (k < 0) k = k + length;
+
+  // No rotatition needed
+  if (k === 0) return head;
+
+  let count = 0;
+  let ptr = head;
+
+  while (count < length - k - 1) {
+    ptr = ptr.next;
+    count++;
+  }
+
+  let partitionHead2 = ptr.next;
+  ptr.next = null;
+  ptr = partitionHead2;
+
+  while (ptr.next !== null) {
+    ptr = ptr.next;
+  }
+
+  ptr.next = head;
+
+  return partitionHead2;
+};
+
+function getLength (head) {
+  let count = 0;
+  while (head !== null) {
+    head = head.next;
+    count++;
+  }
+  return count;
+}
