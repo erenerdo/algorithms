7487b86b29b80293b371989d78e4d942104db50b
Eren Erdogan <ererdogan20@gmail.com>
2018-01-16 16:18:05 -0500

    completed permutations of strings

---
+++ CodeRust 2.0/Math and Stats/PermuteStrings.js
@@ -0,0 +1,25 @@
+/*
+Permute String
+Implement a method to print all permutations of a given string.
+*/
+
+function permuteString (str, curr = '', perms = []) {
+  // Base Case
+  if (str.length === 0) {
+    perms.push(curr);
+    return;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str.slice(i, i + 1);
+    let newCurr = curr.slice();
+    let strCopy = str.slice();
+    strCopy = strCopy.slice(0, i) + strCopy.slice(i + 1);
+    newCurr += char;
+    permuteString(strCopy, newCurr, perms);
+  }
+
+  return perms;
+}
+
+permuteString('bac');
