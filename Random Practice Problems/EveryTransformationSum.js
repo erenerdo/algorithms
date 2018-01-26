b7063335f237d6792102c40c4bbf18b1d811da16
Eren Erdogan <ererdogan20@gmail.com>
2018-01-26 11:05:08 -0500

    cleaned up everytransformation

--- Random Practice Problems/EveryTransformationSum.js
+++ Random Practice Problems/EveryTransformationSum.js
@@ -4,23 +4,23 @@ Create a function that prints every transformation of the string that equals the
 A transformation is: mathemtical expression with + or - mixed in to the every value of the string input
 
 */
-function everyTransformationSum (numStrs, targetSum, currentSum = 0, currentComb = []) {
+function everyTransformationSum (numStrs, targetSum, currentComb = []) {
   // Base Case
-  if (numStrs.length === 0 && currentSum === targetSum) {
-      console.log(currentComb);
+  if (numStrs.length === 0 && !targetSum) {
+      console.log(currentComb.join(''));
   } else if (numStrs.length === 0) {
       return;
   }
 
   for (let i = 0; i < numStrs.length; i++) { // O(N)
-      let currentNumStr = numStrs.slice(0, i+1); // O(N)
-      let remainingStr = numStrs.slice(i+1) // O(N)
+    let currentNumStr = numStrs.slice(0, i+1); // O(N)
+    let remainingStr = numStrs.slice(i+1) // O(N)
 
-      let positiveCurrent = "+" + currentNumStr; // O(N)
-      let negativeCurrent = "-" + currentNumStr; // O(N)
+    let positiveCurrent = "+" + currentNumStr; // O(N)
+    let negativeCurrent = "-" + currentNumStr; // O(N)
 
-      everyTransformationSum(remainingStr, targetSum, currentSum + Number(positiveCurrent), currentComb.concat(positiveCurrent));
-      everyTransformationSum(remainingStr, targetSum, currentSum + Number(negativeCurrent), currentComb.concat(negativeCurrent));
+    everyTransformationSum(remainingStr, targetSum - +(positiveCurrent), currentComb.concat(positiveCurrent));
+    everyTransformationSum(remainingStr, targetSum - +(negativeCurrent), currentComb.concat(negativeCurrent));
   }
 }
 
