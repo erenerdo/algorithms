a92889fe70bb1011dcab4b183a0b65834fe90e94
Eren Erdogan <ererdogan20@gmail.com>
2018-01-26 15:01:12 -0500

    completed max sum subsequence

---
+++ CodeRust 2.0/Dynamic Programming/MaxSumSubsequenceNonAdjacent.js
@@ -0,0 +1,22 @@
+/*
+MaxSum Subsequence - Nonadjacent Elements
+Find an efficient algorithm to find maximum sum of a subsequence in an array such that no consecutive elements are part of this subsequence.
+*/
+
+var rob = function(nums) {
+  // Edge Cases
+  if (!nums || !nums.length) return 0;
+  else if (nums.length === 1) return nums[0];
+  else if (nums.length === 2) return Math.max(nums[0], nums[1]);
+
+  let nMinus2 = nums[0];
+  let nMinus1 = Math.max(nums[0], nums[1]);
+  let curMax;
+
+  for (let i = 2; i < nums.length; i++) {
+    curMax = Math.max(nMinus1, nMinus2 + nums[i]);
+    nMinus2 = nMinus1;
+    nMinus1 = curMax;
+  }
+  return curMax;
+};
